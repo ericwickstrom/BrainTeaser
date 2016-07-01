d4648c94b5f0b7ae3b85b147df3eb143b727371d
Eric Wickstrom <wickstrome@gmail.com>
2016-07-01 11:24:30 -0500

    Added methods to update text views when problem is created. Added methods to check for correct answer and update score. Displays a toast if the user chose the correct or incorrect answer.

--- app/src/main/java/com/example/beardsmcgee/brainteaser/MainActivity.java
+++ app/src/main/java/com/example/beardsmcgee/brainteaser/MainActivity.java
@@ -11,7 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -27,16 +29,16 @@ public class MainActivity extends AppCompatActivity {
     private TextView scoreTextView;
 
     // possible option 1
-    private TextView topLeftTextView;
+    private TextView firstButtonTextView;
 
     // possible option 2
-    private TextView topRightTextView;
+    private TextView secondButtonTextView;
 
     // possible option 3
-    private TextView bottomLeftTextView;
+    private TextView thirdButtonTextView;
 
     // possible option 4
-    private TextView bottomRightTextView;
+    private TextView fourthButtonTextView;
 
     // Upper bound for random ints
     private final int UPPER_BOUND = 9;
@@ -44,52 +46,74 @@ public class MainActivity extends AppCompatActivity {
     //Upper bound for wrong answers
     private final int UPPER_BOUND_ANSWERS = 18;
 
-    /*
-     * Problem is as follows: a + b = answer
+    /* Using these for readability when assigning values
+     * after generating each problem.
      */
+    private final int FIRST_BUTTON = 0;
+    private final int SECOND_BUTTON = 1;
+    private final int THIRD_BUTTON = 2;
+    private final int FOURTH_BUTTON = 3;
+
+
+    // Problem is as follows: a + b = answer
     private int a;
     private int b;
     private int answer;
+
+    // Counts number of total number of questions answered and how many
+    // answered correctly
+    private int totalQuestions;
+    private int totalCorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //********************testing************************
+        Button testButton = (Button) findViewById(R.id.testButton);
+        testButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                createProblem();
+            }
+        });
+
         timerTextView       = (TextView) findViewById(R.id.timerTextView);
         problemTextView     = (TextView) findViewById(R.id.problemTextView);
         scoreTextView       = (TextView) findViewById(R.id.scoreTextView);
-        topLeftTextView     = (TextView) findViewById(R.id.topLeftTextView);
-        topRightTextView    = (TextView) findViewById(R.id.topRightTextView);
-        bottomLeftTextView  = (TextView) findViewById(R.id.bottomLeftTextView);
-        bottomRightTextView = (TextView) findViewById(R.id.bottomRightTextView);
+        firstButtonTextView = (TextView) findViewById(R.id.firstButtonTextView);
+        secondButtonTextView = (TextView) findViewById(R.id.secondButtonTextView);
+        thirdButtonTextView = (TextView) findViewById(R.id.thirdButtonTextView);
+        fourthButtonTextView = (TextView) findViewById(R.id.fourthButtonTextView);
 
-        topLeftTextView.setOnClickListener(new View.OnClickListener(){
+        firstButtonTextView.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                Log.i("button", "topLeft clicked");
+                answer((TextView) view);
             }
         });
 
-        topRightTextView.setOnClickListener(new View.OnClickListener(){
+        secondButtonTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Log.i("button", "topRight clicked");
+                answer((TextView) view);
             }
         });
 
-        bottomLeftTextView.setOnClickListener(new View.OnClickListener(){
+        thirdButtonTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Log.i("button", "bottomLeft clicked");
-            }
+                answer((TextView) view);}
         });
 
-        bottomRightTextView.setOnClickListener(new View.OnClickListener(){
+        fourthButtonTextView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Log.i("button", "bottomRight clicked");
+                answer((TextView) view);
             }
         });
 
@@ -101,7 +125,8 @@ public class MainActivity extends AppCompatActivity {
      * Updates a, b and answer.
      * Generates answers
      */
-    private void generateProblem(){
+    private void createProblem(){
+        totalQuestions++;
         // create problem
         Random generator = new Random();
         a = generator.nextInt(UPPER_BOUND);
@@ -119,7 +144,68 @@ public class MainActivity extends AppCompatActivity {
 
         //todo: correctly formatted strings
         //assign textviews
+        switch(correctButton){
+            case(FIRST_BUTTON):
+                setAnswers(firstButtonTextView, secondButtonTextView, thirdButtonTextView,
+                        fourthButtonTextView, x, y, z);
+                break;
+
+            case(SECOND_BUTTON):
+                setAnswers(secondButtonTextView, firstButtonTextView, thirdButtonTextView,
+                        fourthButtonTextView, x, y, z);
+                break;
+
+            case(THIRD_BUTTON):
+                setAnswers(thirdButtonTextView, firstButtonTextView, secondButtonTextView,
+                        fourthButtonTextView, x, y, z);
+                break;
+
+            case(FOURTH_BUTTON):
+                setAnswers(fourthButtonTextView, firstButtonTextView, secondButtonTextView,
+                        thirdButtonTextView, x, y, z);
+                break;
+        }
+        setProblem();
+
 
     }
 
+
+    // Sets text of other text views to integers
+    // First argument will contain correct answer, rest will be incorrect.
+    // X, Y, Z are incorrect answers
+    private void setAnswers(TextView correctButton, TextView button1, TextView button2, TextView button3, int x, int y, int z){
+        correctButton.setText(String.valueOf(answer));
+        button1.setText(String.valueOf(x));
+        button2.setText(String.valueOf(y));
+        button3.setText(String.valueOf(z));
+    }
+
+    //Sets text of problem
+    private void setProblem(){
+        problemTextView.setText(String.valueOf(a) + " + " + String.valueOf(b));
+    }
+
+    //sets text of scoreTextView
+    private void setScore(){
+        scoreTextView.setText(String.valueOf(totalCorrect) + "/" + String.valueOf(totalQuestions));
+    }
+
+    // Checks if selected answer is correct
+    // calls setScore() and createProblem()
+    private void answer(TextView answerButton){
+        int toastId;
+        if(Integer.parseInt(answerButton.getText().toString()) == answer){
+            totalCorrect++;
+            toastId = R.string.toast_correct;
+        } else {
+            toastId = R.string.toast_incorrect;
+        }
+        //displays a toast notifying user answer is correct or incorrect
+        Toast.makeText(getApplicationContext(), toastId, Toast.LENGTH_SHORT).show();
+
+        //updates score text view and creates next problem
+        setScore();
+        createProblem();
+    }
 }
